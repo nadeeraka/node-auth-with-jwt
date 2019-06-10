b0f9c79a01208100f4501be9f383052ea292ca61
nadeeraka <nimanthanadeeraka@outlook.com>
2019-06-10 19:17:08 +0530

    react nav added

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
 import "bootswatch/dist/superhero/bootstrap.css";
+import Nav from "./components/Nav";
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <p>jjj</p>
     </div>
   );
